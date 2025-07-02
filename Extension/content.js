6040e2ca63a65c06ab6ed80606f5da3c4b7f3480
Saberpeep <3688260+Saberpeep@users.noreply.github.com>
2025-07-01 21:46:40 -0700

    add functionality for locations page

--- Extension/content.js
+++ Extension/content.js
@@ -5,9 +5,18 @@
     const markerClass = "lackofbindings";
     const cardContentSelector = ".card-text";
 
+    const locationsPagePath = "/home/locations";
+    const locationsInstanceLinkSelector = `#app .locations a[href^="/home/launch"]`;
+    const locationRegionBadgeSelector = `div[aria-label="Region Badge"]`;
+
+    const userCache = new Map();
+
     async function getUserDetails(userId)
     {
         try {
+            // Check cache first
+            if(userCache.has(userId)) return userCache.get(userId);
+
             let res = await fetch(`https://vrchat.com/api/1/users/${userId}`, {
                 "credentials": "include",
                 "headers": {
@@ -21,20 +30,23 @@
                 "method": "GET",
                 "mode": "cors"
             });
-            return await res.json();
+            let user = await res.json();
+            // Save result to cache
+            if(user && user.id) userCache.set(user.id, user);
+            return user;
         } catch (error) {
             throw error;
         }
     }
 
-    function extractInfoFromURL()
+    function extractInfoFromURL(urlString)
     {
-        let url = new URL(document.URL);
+        let url = new URL(urlString);
         let instanceID = url.searchParams.get("instanceId");
         if(!instanceID) throw new Error("instanceId not found in URL");
         return {
-            userId: instanceID.match(/usr_[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)[0],
-            region: instanceID.match(/region\((.+?)\)/)[1],
+            userId: instanceID.match(/usr_[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/)?.[0],
+            region: instanceID.match(/region\((.+?)\)/)?.[1],
         }
     }
 
@@ -65,7 +77,7 @@
         return node;
     }
 
-    async function tryAddDetails()
+    async function tryAddLaunchPageDetails()
     {
         try {
             // Look for the area with the details
@@ -76,7 +88,7 @@
             let cardContentOrig = cardBody.querySelector(cardContentSelector);
 
             // Get instance details
-            let { userId, region } = extractInfoFromURL();
+            let { userId, region } = extractInfoFromURL(document.URL);
             let user = await getUserDetails(userId);
 
             // Duplicate and modify with new info
@@ -85,6 +97,47 @@
             // Make another for region info (because its basically free from the URL)
             addDetailsNode(cardContentOrig.cloneNode(true), `Region: ${region.toUpperCase()}`, "", "", cardContentOwner);
 
+        } catch (error) {
+            cleanUp();
+            throw error;
+        }
+    }
+
+    async function tryAddLocationsPageDetails()
+    {
+        try {
+            // Look for each location in the list
+            let instanceLinks = Array.from(document.querySelectorAll(locationsInstanceLinkSelector));
+            console.log("On Locations Page, detected:", instanceLinks);
+
+            for (let link of instanceLinks)
+            {
+                try {
+                    // Find the region item and clone it
+                    let regionItem = link.parentElement.querySelector(locationRegionBadgeSelector).parentElement;
+                    let newItem = regionItem.cloneNode();
+    
+                    // Get instance details
+                    let { userId } = extractInfoFromURL(link.href);
+                    if(!userId) continue; // Might be a group instance or something else
+                    let user = await getUserDetails(userId);
+    
+                    // Modify new element
+                    newItem.classList.add(markerClass);
+                    let newLink = document.createElement("a");
+                    newLink.innerText = user.displayName;
+                    newLink.href = `/home/user/${user.id}`;
+                    newLink.classList.add(markerClass);
+                    newItem.appendChild(newLink);
+    
+                    // Insert after region item
+                    regionItem.parentNode.insertBefore(newItem, regionItem.nextSibling);
+                    
+                } catch (error) {
+                    console.error(error);
+                    continue;
+                }
+            }
         } catch (error) {
             cleanUp();
             throw error;
@@ -106,11 +159,18 @@
     }
 
 
-    // Check to make sure we're on launch page, then modify the page.
-    if(new URL(document.URL).pathname.startsWith(launchPagePath))
+    // Check to make sure we're on launch or locations page, then modify the page.
+    let pathname = new URL(document.URL).pathname;
+    
+    if(pathname.startsWith(launchPagePath))
     {
         cleanUp();
-        tryAddDetails();
+        tryAddLaunchPageDetails();
+    }
+    else if (pathname.startsWith(locationsPagePath))
+    {
+        cleanUp();
+        tryAddLocationsPageDetails();
     }
 
 
